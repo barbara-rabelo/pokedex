8db742dcdfd40e9798895bf4024a6eecccec07ab
Bárbara Rabelo <barbara.rabelo@live.com>
2022-06-08 04:59:51 -0300

    Add files via upload

---
+++ api-test/src/test/java/com/pokedex/apiTest/apiTest.java
@@ -0,0 +1,11 @@
+package com.pokedex.apiTest;
+
+import org.junit.Test;
+
+public class apiTest {
+    @Test
+    public static void search() {
+        String url = "https://www.pokemon.com/us/pokedex/";
+
+    }
+}
